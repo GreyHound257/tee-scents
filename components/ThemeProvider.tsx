f490a7d882f32c18250e561bb3889bae67e812fb
GreyHound257 <dayoobatewoseenioolaoluwa@gmail.com>
2026-05-09 21:14:18 +0100

    Fix ThemeProvider type import for Vercel build

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -1,7 +1,12 @@
 "use client";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+// We use the standard React ComponentProps for the provider instead of the deep path
+export function ThemeProvider({ 
+  children, 
+  ...props 
+}: React.ComponentProps<typeof NextThemesProvider>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
